b042cf051b26c7fe633bb1e1b10fd1b8185f2076
guoguirong <guoguirong@boco.com.cn>
2019-11-07 17:45:53 +0800

    提交

--- leecode/src/main/java/com/ggr/leecode/string/最长回文.java
+++ leecode/src/main/java/com/ggr/leecode/string/最长回文.java
@@ -95,6 +95,22 @@ public class 最长回文 {
         }
         return "";
     }
+    public String solution3(String s) {
+        int n = s.length();
+        String res = "";
+        boolean[][] dp = new boolean[n][n];
+        for(int i = 0 ;i < n; i++){
+            for(int j = i; j >= 0 ;j --){
+                if(s.charAt(i) == s.charAt(j) && ( i - j < 2 || dp[i-1][j+1]))
+                    dp[i][j] = true;
+                if (dp[i][j] && (i - j + 1 > res.length())){
+                    res = s.substring(j,i+1);
+                }
+            }
+        }
+        return res;
+
+    }
 
 
     /**
