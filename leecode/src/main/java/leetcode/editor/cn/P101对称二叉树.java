293041ab41325bdccab52e33d4a62b722225d8e1
郭贵荣 <guoguirong@boco.com.cn>
2020-05-24 23:08:12 +0800

    1

---
+++ leecode/src/main/java/leetcode/editor/cn/P101对称二叉树.java
@@ -0,0 +1,84 @@
+//给定一个二叉树，检查它是否是镜像对称的。 
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+
+package leetcode.editor.cn;
+
+//Java：对称二叉树
+public class P101对称二叉树 {
+    public static void main(String[] args) {
+        Solution solution = new P101对称二叉树().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return this.isSymmetric(root.left, root.right);
+        }
+
+        private boolean isSymmetric(TreeNode t1, TreeNode t2) {
+            if (t1 == null && t2 == null) {
+                return true;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            return isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
